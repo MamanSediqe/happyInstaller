e13beb3a93af722c1a0ec0f74e2e7feee9d07008
Phamtom <158246647+MamanSediqe@users.noreply.github.com>
2024-04-16 14:41:55 +0330

    Update setupDB.sh

--- setupDB.sh
+++ setupDB.sh
@@ -1,3 +1,7 @@
+sudo mysql -u root -e "CREATE USER 'happy_user'@'localhost' IDENTIFIED BY 'Happy_12346';"
+sudo mysql -u root -e "FLUSH PRIVILEGES;"
+sudo mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'happy_user'@'localhost';"
+sudo mysql -u happy_user -p'Happy_12346' -e "CREATE DATABASE IF NOT EXISTS LinksDb;"
 curl -H "Cache-Control: no-cache" -H "Pragma: no-cache" -Ls https://raw.githubusercontent.com/MamanSediqe/happyInstaller/main/DefaultDb.sql > DefaultDb.sql
 sudo mysql -u root < DefaultDb.sql
 rm DefaultDb.sql
