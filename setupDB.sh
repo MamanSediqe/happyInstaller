3f5de32ecaba62f88825c4a75431cc0c72c473ff
Phamtom <158246647+MamanSediqe@users.noreply.github.com>
2024-04-16 14:35:42 +0330

    Update setupDB.sh

--- setupDB.sh
+++ setupDB.sh
@@ -1,2 +1,3 @@
 curl -H "Cache-Control: no-cache" -H "Pragma: no-cache" -Ls https://raw.githubusercontent.com/MamanSediqe/happyInstaller/main/DefaultDb.sql > DefaultDb.sql
 sudo mysql -u root < DefaultDb.sql
+rm DefaultDb.sql
