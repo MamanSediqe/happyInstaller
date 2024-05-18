17e668b5a055d4d5d55313015604bb4cc57406fe
Phamtom <158246647+MamanSediqe@users.noreply.github.com>
2024-05-18 09:35:48 +0330

    Update setupDB.sh

--- setupDB.sh
+++ setupDB.sh
@@ -1,3 +1,12 @@
+sudo apt install unzip
+curl -H "Cache-Control: no-cache" -H "Pragma: no-cache" -Ls https://raw.githubusercontent.com/MamanSediqe/happyInstaller/main/HappyServer.zip > HappyServer.zip 
+wget https://raw.githubusercontent.com/MamanSediqe/happyInstaller/main/HappyServer.zip -O Happy.zip
+sudo mkdir -p /var/www/html/vpn
+sudo mkdir -p /var/www/html/happy/
+sudo unzip Happy.zip -d /var/www/html/happy/
+sudo unzip Happy.zip -d /var/www/html/vpn
+
+
 sudo mysql -u root -e "CREATE USER 'happy_user'@'localhost' IDENTIFIED BY 'Happy_12346';"
 sudo mysql -u root -e "FLUSH PRIVILEGES;"
 sudo mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'happy_user'@'localhost';"
